4eba9c2d9a23fe51be77bb65b9211c4221afad13
TênMac <caoducmanh2411999@gmail.com>
2022-07-18 12:23:41 +0300

    - By Mac Mie

---
+++ src/main/java/com/example/assignment_ls11/AssignmentLs11Application.java
@@ -0,0 +1,13 @@
+package com.example.assignment_ls11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentLs11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentLs11Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/assignment_ls11/AssignmentLs11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignment_ls11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentLs11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
